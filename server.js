372ee85755e8ee78c5b0dfec70130606a66cfb86
AKshay Panchal <akshaypanchal67@gmail.com>
2018-10-02 23:46:02 -0400

    Update Port number for heroku

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.put('/image', (req, res)=>{
 })
 
 
-app.listen(3000, ()=>{
-	console.log('app is running');
+app.listen(process.env.PORT || 3000, ()=>{
+	console.log(`app is running on port ${process.env.PORT}`);
 })
 
